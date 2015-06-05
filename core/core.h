c8facc4c19f7b2f0cd002bb90a91824195f02429
Terence Martin <poindexter@nurdz.com>
2015-06-05 14:39:22 -0700

    Add a single include file for the core
    I like the system where the name of the library and the name of the
    single header you should include are both the same.

---
+++ core/core.h
@@ -0,0 +1,18 @@
+#ifndef __COREdotH__
+#define __COREdotH__
+
+/***********************************************************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include "config.h"
+#include "registers.h"
+#include "opcodes.h"
+#include "context.h"
+#include "vm.h"
+
+/***********************************************************************************************************/
+
+#endif
